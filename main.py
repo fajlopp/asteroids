6c72187c401ee2bcbeb61d3729a2a08d250a1e99
fajlopp <fmoszczynski@gmail.com>
2024-09-04 19:16:36 +0200

    few things

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT  
+
+def main():
+    pygame.init()  
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption('Pygame Window')  
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        
+        screen.fill((0, 0, 0))  
+
+        pygame.display.flip()  
+
+    pygame.quit()  
+
+if __name__ == "__main__":
+    main()
