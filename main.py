be9a4113ea86011264622401350e16f543984e18
fajlopp <fmoszczynski@gmail.com>
2024-09-05 18:47:28 +0200

    using groups

--- main.py
+++ main.py
@@ -5,11 +5,16 @@ from player import *
 def main():
     pygame.init() 
     clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pygame.display.set_caption('Pygame Window')  
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    dt = 0
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    print(f"Player created at position: {player.position}")
 
 
     while True:
@@ -18,9 +23,15 @@ def main():
                 return
 
         
+
+        for obj in updatable:
+            obj.update(dt)
+
         screen.fill((0, 0, 0))
-        player.draw(screen)
-        player.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip() 
         dt = clock.tick(60) / 1000
 
