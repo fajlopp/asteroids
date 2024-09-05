b57d4a3443095ac2d242d9d0fd3c73a97956bb73
fajlopp <fmoszczynski@gmail.com>
2024-09-05 18:05:53 +0200

    player is now on the screen

--- circleshape.py
+++ circleshape.py
@@ -14,7 +14,7 @@ class CircleShape(pygame.sprite.Sprite):
         self.radius = radius
 
     def draw(self, screen):
-        pygame.draw.polygon(screen, 'white', *self.triangle(), 2)
+        pygame.draw.polygon(screen, 'white', self.triangle(), 2)
 
     def update(self, dt):
         # sub-classes must override
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init() 
@@ -7,16 +8,19 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption('Pygame Window')  
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    print(f"Player created at position: {player.position}")
 
-    
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         
-        screen.fill((0, 0, 0))  
-        pygame.display.flip()  
+        screen.fill((0, 0, 0))
+        player.draw(screen)
+        pygame.display.flip() 
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
